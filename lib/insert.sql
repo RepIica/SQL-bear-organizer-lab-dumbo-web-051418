685ba2610e5f725a74f7493330d85dc716308aff
Sammy Akharaz <ketrixz@gmail.com>
2018-05-21 18:28:30 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT (name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", "Rowdy", "Tabitha", "Sergeant Brown", "Melissa", "Grinch", "Wendy", "" )
