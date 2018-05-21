efdb8382e995fa96393c65623660a22abd3d4aef
Sammy Akharaz <ketrixz@gmail.com>
2018-05-21 18:45:06 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES ("Mr. Chocolate", 23, "M", "Black", "Docile", true);
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 23, "M", "Black", "Docile", true);
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 23, "M", "Black", "Docile", true);
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 23, "M", "Black", "Docile", true);
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 23, "M", "Black", "Docile", true);
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 23, "M", "Black", "Docile", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "M", "Black", "Docile", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 23, "M", "Black", "Docile", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 23, "M", "Black", "Docile", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 23, "M", "Black", "Docile", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 23, "M", "Black", "Docile", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 23, "M", "Black", "Docile", 1);
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 23, "M", "Black", "Docile", true);
--- INSERT INTO bears (age, gender, color, temperament, alive) VALUES ( 23, "M", "Black", "Docile", true);
+-- INSERT INTO bears (age, gender, color, temperament, alive) VALUES ( 23, "M", "Black", "Docile", 1);
