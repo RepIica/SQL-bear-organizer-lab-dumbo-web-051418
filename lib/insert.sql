609add17aaf675795a74a0678d5da3d933e84dac
Sammy Akharaz <ketrixz@gmail.com>
2018-05-21 18:45:14 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Ch
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 23, "M", "Black", "Docile", 1);
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 23, "M", "Black", "Docile", 1);
 -- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 23, "M", "Black", "Docile", 1);
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 23, "M", "Black", "Docile", true);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 23, "M", "Black", "Docile", 1);
 -- INSERT INTO bears (age, gender, color, temperament, alive) VALUES ( 23, "M", "Black", "Docile", 1);
